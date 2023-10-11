88473a576bd73906d4d2da638656168a3612ca62
nitinryali <rnssrinivas1@gmail.com>
2023-10-11 19:48:54 +0530

    updated navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,49 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
+    // Function to close the navbar when a link is clicked
+    const closeNavbar = () => {
+        setIsNavOpen(false);
+    };
+
     return (
         <>
-            <div className="navbar active sticky-top navbar-expand-lg navbar navbar-light navcus">
+            <div className={`navbar active sticky-top navbar-expand-lg navbar navbar-light navcus ${isNavOpen ? 'active' : ''}`}>
                 <div className="container-fluid">
-                    <a className="navbar-brand"><img src="./img/logo.png" height="35" alt="" loading="lazy" style={{backgroundColor: 'transparent'}}/></a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className="navbar-brand">
+                        <img src="./img/logo.png" height="35" alt="" loading="lazy" style={{ backgroundColor: 'transparent' }} />
+                    </a>
+                    <button className="navbar-toggler" type="button" onClick={toggleNav}>
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                        <div className="navbar-nav ">
-                            <Link to='/' className="fw-bold nav-link active" aria-current="page">Home</Link>
-                            <Link to='/app' className="fw-bold nav-link active" aria-current="page">App</Link>
-                            <Link to='/team' className="fw-bold nav-link active" aria-current="page">Team</Link>
-                            <Link to='/blogs' className="fw-bold nav-link active" aria-current="page">Blogs</Link>
-                            <Link to='/contact' className="fw-bold nav-link active" aria-current="page">Contact</Link>
+                    <div className={`collapse navbar-collapse justify-content-end ${isNavOpen ? 'show' : ''}`} id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <Link to='/' className="fw-bold nav-link active" aria-current="page" onClick={closeNavbar}>
+                                Home
+                            </Link>
+                            <Link to='/app' className="fw-bold nav-link active" aria-current="page" onClick={closeNavbar}>
+                                App
+                            </Link>
+                            <Link to='/team' className="fw-bold nav-link active" aria-current="page" onClick={closeNavbar}>
+                                Team
+                            </Link>
+                            <Link to='/blogs' className="fw-bold nav-link active" aria-current="page" onClick={closeNavbar}>
+                                Blogs
+                            </Link>
+                            <Link to='/contact' className="fw-bold nav-link active" aria-current="page" onClick={closeNavbar}>
+                                Contact
+                            </Link>
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
