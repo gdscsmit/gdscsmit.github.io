910019eb0a83f1c7d43437d251746c64fe1eb378
Govind Nair <nairgovind01@gmail.com>
2023-10-12 23:53:32 +0530

    added .env for firebase-secret

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,11 +5,11 @@ import  "firebase/compat/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAif4F5mi3fMBQ870S-GkgGf_B5VoPleZI",
-  authDomain: "contact-page-1881c.firebaseapp.com",
-  projectId: "contact-page-1881c",
-  storageBucket: "contact-page-1881c.appspot.com",
-  messagingSenderId: "913481427578",
+  apiKey:"{process.env.REACT_APP_FIREBASE_API_KEY}",
+  authDomain:"{process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}",
+  projectId:"{process.env.REACT_APP_FIREBASE_PROJECT_ID}",
+  storageBucket:"{process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId:"{process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}",
   appId: "1:913481427578:web:87166746defa20235029c3"
 };
 
