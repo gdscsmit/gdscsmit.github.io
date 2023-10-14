89344624cf00ee7cbe09254062035acbf1c2f66b
pooranjoyb <pooranjoyb2016@gmail.com>
2023-10-15 01:40:19 +0530

    Teams Page Updation v1

---
+++ src/constants/TeamImages.js
@@ -0,0 +1,31 @@
+import { storage } from '../firebaseConfig';
+
+const getAllFilesFromStorage = async () => {
+  try {
+    const fileUrls = [];
+    const storageRef = storage.ref();
+
+    const files = await storageRef.listAll();
+
+    for (const file of files.items) {
+      const url = await file.getDownloadURL();
+      fileUrls.push({ name: file.name, url });
+    }
+    return fileUrls;
+
+  } catch (error) {
+    console.error('Error fetching files from storage:', error);
+    throw error;
+  }
+
+};
+
+export async function fetchFiles() {
+    try {
+      const files = await getAllFilesFromStorage();
+      console.log(files)
+      return files
+    } catch (error) {
+      console.error('Error fetching files:', error);
+    }
+}
