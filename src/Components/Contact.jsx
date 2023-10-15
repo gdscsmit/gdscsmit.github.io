fc6821e31251c07dacea8ed2f399a9d31eaa638c
Ashesh <asheshmandal73@gmail.com>
2023-10-15 15:17:45 +0530

    Modified contact page UI

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,63 +1,115 @@
-import React, { useState } from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import { database } from '../firebaseConfig';
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { database } from "../firebaseConfig";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
 
+    database
+      .ref("Responses")
+      .push({
+        name: name,
+        email: email,
+        message: message,
+      })
+      .catch((error) => console.error("error submitting data :", error));
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(name, email, message);
+    alert("Sent Successfully!");
+  };
+  return (
+    <>
+      <Navbar />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div className="col-md-4 mb-5 mt-4 contact_logo">
+          <img src="./img/gdsc_form.svg" className="img-fluid" alt="..." />
+        </div>
 
-        database.ref("Responses").push({
-            name: name,
-            email: email,
-            message: message
-        }).catch((error) => console.error('error submitting data :', error));
+        <div
+          className="row card shadow d-flex align-items-center flex-row p-5 mb-3"
+          style={{ width: "80%", height: "auto" }}
+        >
+          <div className="col-md-6 p-5">
+            <img
+              className="img-fluid"
+              src="https://img.freepik.com/free-vector/flat-design-illustration-customer-support_23-2148887720.jpg?w=740&t=st=1697360648~exp=1697361248~hmac=c1048c6b6eabd19c22ce4b952e7408ee9bed89a1ee6505893c371e26c3849053"
+              alt=""
+            />
+          </div>
+          <div className="col-md-6 col-sm-12 card p-4">
+            <p className="align-self-center h3 fw-bold text-primary mb-4">
+              Contact Us
+            </p>
+            <form onSubmit={handleSubmit}>
+              <div className="form-outline input-group mb-4 ">
+                <span class="input-group-text" id="basic-addon1">
+                  <i class="fa-solid fa-user text-primary"></i>
+                </span>
+                <input
+                  type="text"
+                  id="form4Example1"
+                  className="form-control"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(event) => {
+                    setName(event.target.value);
+                  }}
+                />
+              </div>
 
-        alert("Sent Successfully!")
+              <div className="form-outline input-group mb-4 ">
+                <span class="input-group-text" id="basic-addon1">
+                  <i class="fa-solid fa-envelope text-primary"></i>
+                </span>
+                <input
+                  type="email"
+                  id="form4Example2"
+                  className="form-control"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                />
+              </div>
 
-    };
-    return (
-        <>
-            <Navbar />
-            <div className="col-md-4 my-4 mx-auto contact_logo">
-                <img src="./img/gdsc_form.svg" className="img-fluid" alt="..." />
-            </div>
+              <div className="form-outline mb-4">
+                <textarea
+                  className="form-control"
+                  id="form4Example3"
+                  rows="4"
+                  value={message}
+                  placeholder="Type your message here..."
+                  onChange={(event) => {
+                    setMessage(event.target.value);
+                  }}
+                ></textarea>
+              </div>
 
-            <form className='my-5 w-50 mx-auto' onSubmit={handleSubmit}>
-                <div className="form-outline mb-4">
-                    <label className="form-label" htmlFor="form4Example1">Name</label>
-                    <input type="text" id="form4Example1" className="form-control" value={name} onChange={(event) => {
-                        setName(event.target.value)
-                    }} />
-                </div>
-
-                <div className="form-outline mb-4 ">
-                    <label className="form-label" htmlFor="form4Example2">Email address</label>
-                    <input type="email" id="form4Example2" className="form-control" value={email} onChange={(event) => {
-                        setEmail(event.target.value)
-                    }} />
-                </div>
-
-                <div className="form-outline mb-4">
-                    <label className="form-label" htmlFor="form4Example3">Message</label>
-                    <textarea className="form-control" id="form4Example3" rows="4" value={message} onChange={(event) => {
-                        setMessage(event.target.value)
-                    }} ></textarea>
-                </div>
-
-                <div className='d-flex justify-content-center'>
-                    <button type="submit" className="btn btn-primary mb-4" >Send</button>
-                </div >
+              <div className="d-flex justify-content-center">
+                <button type="submit" className="btn btn-primary w-100 mb-4">
+                  Send
+                </button>
+              </div>
             </form>
-
-            <Footer />
-        </>
-    )
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/database';
-import "firebase/compat/storage";
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/database';
+// import "firebase/compat/storage";
 
-const firebaseConfig = {
-  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
-  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}`,
-  databaseURL: `${import.meta.env.VITE_FIREBASE_DATABASE_URL}`,
-  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${import.meta.env.VITE_FIREBASE_APP_ID}`,
-  measurementId: `${import.meta.env.VITE_FIREBASE_MEASUREMENT_ID}`
-};
+// const firebaseConfig = {
+//   apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+//   authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
+//   projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}`,
+//   databaseURL: `${import.meta.env.VITE_FIREBASE_DATABASE_URL}`,
+//   storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
+//   messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
+//   appId: `${import.meta.env.VITE_FIREBASE_APP_ID}`,
+//   measurementId: `${import.meta.env.VITE_FIREBASE_MEASUREMENT_ID}`
+// };
 
-// Initialize Firebase
+// // Initialize Firebase
 
-firebase.initializeApp(firebaseConfig)
-export const database = firebase.database();
-export const storage = firebase.storage();
+// firebase.initializeApp(firebaseConfig)
+// export const database = firebase.database();
+// export const storage = firebase.storage();
