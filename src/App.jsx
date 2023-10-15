95d5ac8d6e61b5ece2d785eeb51449207d4ecc50
petsamuel <petsamuel4@gmail.com>
2023-10-15 12:15:21 +0100

    loader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
 import Home from './Components/Home'
 import Navbar from './Components/Navbar'
+import { Loader } from './Components/Loader';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
-      <Navbar />
-      <Home/>
-    </>
+      {
+        loading ?
+          <Loader /> : <>
+            <Navbar />
+            <Home />
+          </>
+      }</>
   )
 }
 
